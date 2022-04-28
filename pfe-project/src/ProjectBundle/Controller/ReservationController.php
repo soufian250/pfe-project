4408f6242ef78eda4cc505d02fdced19f48beac2
soufian <elhabib.soufian@gmail.com>
2022-04-28 17:51:35 +0000

    reservation

--- pfe-project/src/ProjectBundle/Controller/ReservationController.php
+++ pfe-project/src/ProjectBundle/Controller/ReservationController.php
@@ -4,14 +4,16 @@ namespace ProjectBundle\Controller;
 
 use Doctrine\ORM\EntityManagerInterface;
 use ProjectBundle\Entity\Car;
+use ProjectBundle\Entity\Reservation;
 use ProjectBundle\Form\CarType;
+use ProjectBundle\Form\ReservationType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class CarController extends Controller
+class ReservationController extends Controller
 {
 
 
@@ -23,36 +25,35 @@ class CarController extends Controller
 
     public function showAction(): ?Response
     {
-        return  $this->render('@Project/Car/show.html.twig');
+        return  $this->render('@Project/Reservation/show.html.twig');
     }
 
     public function addAction(Request $request): ?Response
     {
 
 
-        $car = new Car();
+        $reservation = new Reservation();
 
-        $form = $this->createForm(CarType::class,$car);
+        $form = $this->createForm(ReservationType::class,$reservation);
 
         $form->handleRequest($request);
 
 
         if ($form->isSubmitted() && $form->isValid()) {
 
-            $file = $request->files->get('post');
 
-
-            $car = $form->getData();
+            $reservation = $form->getData();
 
             $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($car);
+            $entityManager->persist($reservation);
             $entityManager->flush();
 
             return $this->redirectToRoute('index_page');
+
         }
 
 
-        return $this->render('@Project/Car/add.html.twig', [
+        return $this->render('@Project/Reservation/add.html.twig', [
             'form' => $form->createView(),
         ]);
 
