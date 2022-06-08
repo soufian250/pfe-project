e3f314724ee6bc140df0c15657c89f42caf1d201
BaddiZineb <zineb1994bd@gmail.com>
2022-06-08 22:33:55 +0100

    contact form

--- pfe-project/src/HomeBundle/Controller/HomeController.php
+++ pfe-project/src/HomeBundle/Controller/HomeController.php
@@ -10,6 +10,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\ORM\QueryBuilder;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Session\Flash\FlashBag;
+
+use HomeBundle\Entity\Contact;
 
 class HomeController extends Controller
 {
@@ -45,8 +55,35 @@ class HomeController extends Controller
     /**
      * @Route("/contact", name="contact")
      */
-    public function contactAction()
+    public function contactAction(Request $request)
     {
-        return  $this->render('@Home/Home/contact.html.twig');
+        $contact = new Contact;     
+     # Add form fields
+       $form = $this->createFormBuilder($contact)
+       ->add('name', TextType::class, array('label'=> 'name', 'attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
+       ->add('email', EmailType::class, array('label'=> 'email','attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
+       ->add('subject', TextType::class, array('label'=> 'subject','attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
+       ->add('message', TextareaType::class, array('label'=> 'message','attr' => array('rows'=>'10','class' => 'form-control')))
+       ->add('Save', SubmitType::class, array('label'=> 'Envoyer', 'attr' => array('class' => 'btn btn-primary', 'style' => 'margin-top:15px')))
+       ->getForm();
+     # Handle form response
+       $form->handleRequest($request);
+
+       if($form->isSubmitted() &&  $form->isValid()){
+        $name = $form['name']->getData();
+        $email = $form['email']->getData();
+        $subject = $form['subject']->getData();
+        $message = $form['message']->getData(); 
+  # set form data   
+        $contact->setName($name);
+        $contact->setEmail($email);          
+        $contact->setSubject($subject);     
+        $contact->setMessage($message);                
+   # finally add data in database
+        $sn = $this->getDoctrine()->getManager();      
+        $sn -> persist($contact);
+        $sn -> flush();}
+        return  $this->render('@Home/Home/contact.html.twig',['form' => $form->createView()]);
     }
+
 }
